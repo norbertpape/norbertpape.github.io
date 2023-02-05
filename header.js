66273c90dbc4f33893cffb78c2c1914c9bb0a9f5
spaceaffairs <spaceaffairs.iasa@gmail.com>
2023-02-05 15:39:38 +0100

    updated news section

--- header.js
+++ header.js
@@ -3,6 +3,7 @@
     <title>Norbert Pape</title>\
     <link rel="stylesheet" href="styles/style.css" >\
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
+    <link rel="canonical" href="https://norbertpape.github.io/" />\
     <meta name="viewport" content="width=device-width, initial-scale=1.0">\
     <meta name="description" content="Personal website of Norbert Pape">\
     <meta name="keywords" content="dance, art, choreography, VR, performance, augmented reality, contemporary art, virtual reality, unity, quest, oculus, pedagogy, self-organisation" />\
