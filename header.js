e85d6ee9bc9b0138d7aa7b6acae976366ac6b447
norbertpape <norbertpape111@gmail.com>
2023-07-23 12:30:03 +0200

    one last LaTex one

--- header.js
+++ header.js
@@ -7,18 +7,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">\
     <meta name="description" content="Personal website of Norbert Pape">\
     <meta name="keywords" content="dance, art, choreography, VR, performance, augmented reality, contemporary art, virtual reality, unity, quest, oculus, pedagogy, self-organisation" />\
-    <script type="text/x-mathjax-config"> MathJax.Hub.Config({ tex2jax: { inlineMath: [[\'$\', \'$\'], [\'\\(\', \'\\)\']] } }); </script >\
+    <script type="text/x-mathjax-config"> MathJax.Hub.Config({ tex2jax: { inlineMath: [[\'$\', \'$\']] } }); </script >\
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML"> </script> </head>');
-
-
-//document.write('<head>\
-//    <meta charset="utf-8">\
-//    <title>Norbert Pape</title>\
-//    <link rel="stylesheet" href="styles/style.css" >\
-//    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
-//    <link rel="canonical" href="https://norbertpape.github.io/" />\
-//    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-//    <meta name="description" content="Personal website of Norbert Pape">\
-//    <meta name="keywords" content="dance, art, choreography, VR, performance, augmented reality, contemporary art, virtual reality, unity, quest, oculus, pedagogy, self-organisation" />\
-//  <script type="text/javascript" async src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML" > </script >\
-//  </head>');
