409943c14bf980847d9a2f9946f09cd9618064e5
norbertpape <norbertpape111@gmail.com>
2023-08-09 16:15:46 +0200

    sitemap and searchability things

--- header.js
+++ header.js
@@ -6,6 +6,6 @@
     <link rel=\"canonical\" href=\"https://norbertpape.github.io/\" />\
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
     <meta name=\"description\" content=\"Personal website of Norbert Pape\">\
-    <meta name=\"keywords\" content=\"dance, art, choreography, VR, performance, augmented reality, contemporary art, virtual reality, unity, quest, oculus, pedagogy, self-organisation\" />\
+    <meta name=\"keywords\" content=\"Norbert Pape, dance, art, choreography, VR, performance, augmented reality, contemporary art, virtual reality, unity, quest, oculus, pedagogy, self-organisation\" />\
     <script type=\"text/x-mathjax-config\"> MathJax.Hub.Config({ tex2jax: { inlineMath: [[\'$\', \'$\']] } }); </script >\
     <script type=\"text/javascript" src=\"https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"> </script> </head>');
